bd65e44df7e535401bd1635a9ec3ce588aacfb16
Brian <sigilwar96@gmail.com>
2023-04-23 11:58:28 -0500

    Project Structure

---
+++ db/main.py
@@ -0,0 +1,16 @@
+from src.auxiliary.connection import connect_to_db
+from src.auxiliary.create import create_price_data_table
+from src.auxiliary.insert import insert_price_data
+
+from src.pricedata.csv_files import csv_filepaths
+
+
+# ---------- Main Function (Create Tables & Insert Data) ---------- #
+def database_main():
+    with connect_to_db() as db_connection:
+        create_price_data_table(db_connection)
+        insert_price_data(db_connection, csv_filepaths)
+
+
+if __name__ == '__main__':
+    database_main()
