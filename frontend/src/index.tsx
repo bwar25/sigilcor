0e95d89ebe2a97d508db7b40bcafc2ff1f374092
Brian <sigilwar96@gmail.com>
2023-04-26 18:42:59 -0500

    styling file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ThemeProvider from './contexts/ThemeProvider';
 
-import './tailwind.css';
-
 
 ReactDOM.render(
   <React.StrictMode>
